d9d3262ee9b316ff95d91ee2d09796c2730fb67a
danielm117 <danielm1103@gmail.com>
2017-07-31 00:54:32 -0500

    finish funtional development

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"regexp"
+	"strconv"
 )
 
 type Game struct {
@@ -47,8 +48,6 @@ func NextMoveHandler(w http.ResponseWriter, r *http.Request)  {
 	//check pieces distribution
 	j:= 7 //end of row
 	for i:=0;i<len(game.Board);i++ {
-		fmt.Print(string(game.Board[i]))
-		fmt.Print(i)
 		if i != j {
 			if (string(game.Board[i]) != "0")&& (string(game.Board[i+1]) != "0"){
 				//check right piece
@@ -80,11 +79,94 @@ func NextMoveHandler(w http.ResponseWriter, r *http.Request)  {
 	}
 
 	if validGame {
-		w.Write([]byte("valid\n"))
-	//	log.Println("valid\n")
+
+		output := ""
+		valid_move := ""
+		end_row := 7
+		start_row := 0
+		player := game.Player
+		opponent := "w"
+		if player == "w" {
+			opponent = "b"
+		}
+		empty_place := "0"
+		board := game.Board
+
+		if player == "b" {
+			for i := start_row; i < len(board); i++ {
+				if string(board[i]) == player {
+					if i != start_row {
+						//check corner left
+						if i+7 < len(board) && string(board[i+7]) == opponent {
+							if i+14 < len(board) && (i+15)%8 != 0 && string(board[i+14]) == empty_place {
+								output = strconv.Itoa(i+1) + "-" + strconv.Itoa(i+15)
+								break
+							}
+						} else if i+7 < len(board) && string(board[i+7]) == empty_place && valid_move == "" {
+							valid_move = strconv.Itoa(i+1) + "-" + strconv.Itoa(i+8)
+						}
+					} else {
+						start_row += 8
+					}
+					if i != end_row {
+						//check corner right
+						if i+9 < len(board) && string(board[i+9]) == opponent {
+							if i+18 < len(board) && (i+18)%8 != 0 && (string(board[i+18]) == empty_place) {
+								output = strconv.Itoa(i+1) + "-" + strconv.Itoa(i+19)
+								break
+							}
+						} else if i+9 < len(board) && string(board[i+9]) == empty_place && valid_move == "" {
+							valid_move = strconv.Itoa(i+1) + "-" + strconv.Itoa(i+10)
+						}
+					} else {
+						end_row += 8
+					}
+				}
+			}
+		} else {
+			for i := start_row; i < len(board); i++ {
+				if string(board[i]) == player {
+					if i != end_row {
+						//check corner right
+						if i-7 >= 0 && string(board[i-7]) == opponent {
+							if i-14 >= 0 && (i-14)%8 != 0 && (string(board[i-14]) == empty_place) {
+								output = strconv.Itoa(i+1) + "-" + strconv.Itoa(i-13)
+								break
+							}
+						} else if i-7 >= 0 && string(board[i-7]) == empty_place && valid_move == "" {
+							valid_move = strconv.Itoa(i+1) + "-" + strconv.Itoa(i-6)
+						}
+					} else {
+						end_row += 8
+					}
+					if i != start_row {
+						//check corner left
+						if i-9 >= 0 && string(board[i-9]) == opponent {
+							if i-18 >= 0 && (i-17)%8 != 0 && string(board[i-18]) == empty_place {
+								output = strconv.Itoa(i+1) + "-" + strconv.Itoa(i-17)
+								break
+							}
+						} else if i-9 >= 0 && string(board[i-9]) == empty_place && valid_move == "" {
+							valid_move = strconv.Itoa(i+1) + "-" + strconv.Itoa(i-8)
+						}
+					} else {
+						start_row += 8
+					}
+				}
+			}
+		}
+		if output != "" {
+		//	fmt.Println(output)
+			w.Write([]byte(output))
+		} else if valid_move != "" {
+		//	fmt.Println(valid_move)
+			w.Write([]byte(valid_move))
+		} else {
+			w.Write([]byte("No se encontro movimiento posible"))
+		}
 	}
-//	w.WriteHeader(http.StatusOK)
-//	w.Write()
+
+	//	w.WriteHeader(http.StatusOK)
 
 }
 
