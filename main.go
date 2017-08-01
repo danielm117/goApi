9671e0c9478a09285c05b522b522b227e277e4ca
danielm117 <danielm1103@gmail.com>
2017-07-31 22:05:31 -0500

    separete in funtions

--- main.go
+++ main.go
@@ -15,159 +15,211 @@ type Game struct {
 		Board string `json:"board"`
 		Player string `json:"player"`
 }
+var (
+	player1 string = "b"
+	player2 string = "w"
+	empty_place string = "0"
+)
 
-func NextMoveHandler(w http.ResponseWriter, r *http.Request)  {
+func CheckBoardHandler(w http.ResponseWriter, r *http.Request)  {
 	var game Game
-	validGame := true
 	w.Header().Set("Content-Type", "text/plain")
 	err := json.NewDecoder(r.Body).Decode(&game)
 	if err != nil {
 		w.WriteHeader(http.StatusNotAcceptable)
 	}
-	//check board size
-	if len(game.Board) != 64 {
-		validGame = false
-		w.Write([]byte("invalid board size\n"))
-	}
 
-	//check game pieces
-	pattern_w := regexp.MustCompile("w")
-	matches_w := len(pattern_w.FindAllStringIndex(game.Board, -1))
-	if matches_w > 12 {
-		validGame = false
-		w.Write([]byte("invalid number of w on board\n"))
-	//	log.Println("invalid number of w on board\n")
-	}
-	pattern_b := regexp.MustCompile("b")
-	matches_b := len(pattern_b.FindAllStringIndex(game.Board, -1))
-	if matches_b > 12 {
-		validGame = false
-		w.Write([]byte("invalid number of b on board\n"))
-	//	log.Println("invalid number of b on board\n")
-	}
-	//check pieces distribution
-	j:= 7 //end of row
-	for i:=0;i<len(game.Board);i++ {
-		if i != j {
-			if (string(game.Board[i]) != "0")&& (string(game.Board[i+1]) != "0"){
-				//check right piece
-				w.Write([]byte("invalid distribution of pieces on board\n"))
-				validGame = false
-				break
-			}else if (len(game.Board)>i+8)&&(string(game.Board[i]) != "0")&& (string(game.Board[i+8]) != "0"){
-				//check button piece
-				w.Write([]byte("invalid distribution of pieces on board\n"))
-				validGame = false
-				break
-			}
-		} else {
-			j+=8
-			//check button piece
-			if (j<=len(game.Board)) && (string(game.Board[i]) != "0")&& (string(game.Board[j]) != "0")  {
-				w.Write([]byte("invalid distribution of pieces on board\n"))
-				validGame = false
-				break
-			}
-		}
-	}
-
-	//check valid player
-	if game.Player != "w" && game.Player != "b" {
-		validGame = false
+	validBoardFormat := ValidateBoardFormat(game.Board)
+	validPiecesDistribution := ValidatePiecesDistribution(game.Board)
+	validPlayer := ValidatePlayer(game.Player)
+	if !validBoardFormat{
+		w.Write([]byte("invalid board format"))
+	}else if !validPlayer {
 		w.Write([]byte("invalid player"))
-	//	log.Println("invalid player")
+	} else if !validPiecesDistribution {
+		w.Write([]byte("invalid distribution of pieces on board"))
+	}else {
+		w.Write([]byte("Perfect board"))
+	}
+}
+
+func NextMoveHandler(w http.ResponseWriter, r *http.Request)  {
+	var game Game
+	w.Header().Set("Content-Type", "text/plain")
+	err := json.NewDecoder(r.Body).Decode(&game)
+	if err != nil {
+		w.WriteHeader(http.StatusNotAcceptable)
 	}
 
-	if validGame {
+	validBoardFormat := ValidateBoardFormat(game.Board)
+	validPiecesDistribution := ValidatePiecesDistribution(game.Board)
+	validPlayer := ValidatePlayer(game.Player)
 
-		output := ""
-		valid_move := ""
-		end_row := 7
-		start_row := 0
-		player := game.Player
-		opponent := "w"
-		if player == "w" {
-			opponent = "b"
+	if validBoardFormat && validPiecesDistribution && validPlayer {
+		var output string
+		if game.Player == player1 {
+			output = SearchMovePlayer1(game)
+		} else {
+			output = SearchMovePlayer2(game)
 		}
-		empty_place := "0"
-		board := game.Board
+		w.Write([]byte(output))
+	}else {
+		w.Write([]byte("Check inputs with others services "))
+	}
+}
 
-		if player == "b" {
-			for i := start_row; i < len(board); i++ {
-				if string(board[i]) == player {
-					if i != start_row {
-						//check corner left
-						if i+7 < len(board) && string(board[i+7]) == opponent {
-							if i+14 < len(board) && (i+15)%8 != 0 && string(board[i+14]) == empty_place {
-								output = strconv.Itoa(i+1) + "-" + strconv.Itoa(i+15)
-								break
-							}
-						} else if i+7 < len(board) && string(board[i+7]) == empty_place && valid_move == "" {
-							valid_move = strconv.Itoa(i+1) + "-" + strconv.Itoa(i+8)
-						}
-					} else {
-						start_row += 8
-					}
-					if i != end_row {
-						//check corner right
-						if i+9 < len(board) && string(board[i+9]) == opponent {
-							if i+18 < len(board) && (i+18)%8 != 0 && (string(board[i+18]) == empty_place) {
-								output = strconv.Itoa(i+1) + "-" + strconv.Itoa(i+19)
-								break
-							}
-						} else if i+9 < len(board) && string(board[i+9]) == empty_place && valid_move == "" {
-							valid_move = strconv.Itoa(i+1) + "-" + strconv.Itoa(i+10)
-						}
-					} else {
-						end_row += 8
-					}
-				}
+
+func ValidateBoardFormat(board string) bool {
+
+	patternPlayer1 := regexp.MustCompile(player1)
+	matchesPlayer1 := len(patternPlayer1.FindAllStringIndex(board, -1))
+	patternEmptyPlace := regexp.MustCompile(empty_place)
+	matchesEmptyPlace := len(patternEmptyPlace.FindAllStringIndex(board, -1))
+	patternPlayer2 := regexp.MustCompile(player2)
+	matchesPlayer2 := len(patternPlayer2.FindAllStringIndex(board, -1))
+
+	//check pieces number
+	if matchesPlayer1 > 12 || matchesPlayer2 > 12 || (matchesPlayer1+matchesPlayer2+matchesEmptyPlace)!=64 {
+		return false
+	} else {
+		return true
+	}
+}
+
+func ValidatePiecesDistribution(board string) bool {
+	is_valid := true
+	end_row:= 7
+
+	for i:=0;i<len(board);i++ {
+		if i != end_row {
+			if (string(board[i]) != empty_place)&& (string(board[i+1]) != empty_place){
+				//check right piece
+				is_valid = false
+				break
+			}else if (len(board)>i+8)&&(string(board[i]) != empty_place)&& (string(board[i+8]) != empty_place){
+				//check button piece
+				is_valid = false
+				break
 			}
 		} else {
-			for i := start_row; i < len(board); i++ {
-				if string(board[i]) == player {
-					if i != end_row {
-						//check corner right
-						if i-7 >= 0 && string(board[i-7]) == opponent {
-							if i-14 >= 0 && (i-14)%8 != 0 && (string(board[i-14]) == empty_place) {
-								output = strconv.Itoa(i+1) + "-" + strconv.Itoa(i-13)
-								break
-							}
-						} else if i-7 >= 0 && string(board[i-7]) == empty_place && valid_move == "" {
-							valid_move = strconv.Itoa(i+1) + "-" + strconv.Itoa(i-6)
-						}
-					} else {
-						end_row += 8
-					}
-					if i != start_row {
-						//check corner left
-						if i-9 >= 0 && string(board[i-9]) == opponent {
-							if i-18 >= 0 && (i-17)%8 != 0 && string(board[i-18]) == empty_place {
-								output = strconv.Itoa(i+1) + "-" + strconv.Itoa(i-17)
-								break
-							}
-						} else if i-9 >= 0 && string(board[i-9]) == empty_place && valid_move == "" {
-							valid_move = strconv.Itoa(i+1) + "-" + strconv.Itoa(i-8)
-						}
-					} else {
-						start_row += 8
-					}
-				}
+			end_row+=8
+			//check button piece
+			if (end_row<=len(board)) && (string(board[i]) != empty_place)&& (string(board[end_row]) != empty_place)  {
+				is_valid = false
+				break
 			}
-		}
-		if output != "" {
-		//	fmt.Println(output)
-			w.Write([]byte(output))
-		} else if valid_move != "" {
-		//	fmt.Println(valid_move)
-			w.Write([]byte(valid_move))
-		} else {
-			w.Write([]byte("No se encontro movimiento posible"))
 		}
 	}
+	return is_valid
+}
 
-	//	w.WriteHeader(http.StatusOK)
+func ValidatePlayer(player string) bool {
+	//check valid player
+	if player != player1 && player != player2 {
+		return false
+	}else {
+		return true
+	}
+}
 
+func SearchMovePlayer1(game Game) string {
+	//the player 1 move down on the board
+	player := game.Player
+	board := game.Board
+	opponent := player2
+
+	output := ""
+	valid_move := ""
+	end_row := 7
+	start_row := 0
+
+	for i := start_row; i < len(board); i++ {
+		if string(board[i]) == player {
+			if i != start_row {
+				//check corner left
+				if i+7 < len(board) && string(board[i+7]) == opponent {
+					if i+14 < len(board) && (i+15)%8 != 0 && string(board[i+14]) == empty_place {
+						output = strconv.Itoa(i+1) + "-" + strconv.Itoa(i+15)
+						break
+					}
+				} else if i+7 < len(board) && i%8 != 0 &&  string(board[i+7]) == empty_place && valid_move == "" {
+					valid_move = strconv.Itoa(i+1) + "-" + strconv.Itoa(i+8)
+				}
+			} else {
+				start_row += 8
+			}
+			if i != end_row {
+				//check corner right
+				if i+9 < len(board) && string(board[i+9]) == opponent {
+					if i+18 < len(board) && (i+18)%8 != 0 && (string(board[i+18]) == empty_place) {
+						output = strconv.Itoa(i+1) + "-" + strconv.Itoa(i+19)
+						break
+					}
+				} else if i+9 < len(board) && (i+1)%8 != 0 && string(board[i+9]) == empty_place && valid_move == "" {
+					valid_move = strconv.Itoa(i+1) + "-" + strconv.Itoa(i+10)
+				}
+			} else {
+				end_row += 8
+			}
+		}
+	}
+	if output != "" {
+		return output
+	} else if valid_move != "" {
+		return valid_move
+	} else {
+		return "No found movements"
+	}
+}
+
+func SearchMovePlayer2(game Game) string {
+	//the player 2 move to up on board
+	player := game.Player
+	board := game.Board
+	opponent := player1
+
+	output := ""
+	valid_move := ""
+	end_row := 7
+	start_row := 0
+
+	for i := start_row; i < len(board); i++ {
+		if string(board[i]) == player {
+			if i != end_row {
+				//check corner right
+				if i-7 >= 0 && string(board[i-7]) == opponent {
+					if i-14 >= 0 && (i-14)%8 != 0 && (string(board[i-14]) == empty_place) {
+						output = strconv.Itoa(i+1) + "-" + strconv.Itoa(i-13)
+						break
+					}
+				} else if i-7 >= 0 && (i-7)%8 != 0 && string(board[i-7]) == empty_place && valid_move == "" {
+					valid_move = strconv.Itoa(i+1) + "-" + strconv.Itoa(i-6)
+				}
+			} else {
+				end_row += 8
+			}
+			if i != start_row {
+				//check corner left
+				if i-9 >= 0 && string(board[i-9]) == opponent {
+					if i-18 >= 0 && (i-17)%8 != 0 && string(board[i-18]) == empty_place {
+						output = strconv.Itoa(i+1) + "-" + strconv.Itoa(i-17)
+						break
+					}
+				} else if i-9 >= 0 && i%8 != 0 && string(board[i-9]) == empty_place && valid_move == "" {
+					valid_move = strconv.Itoa(i+1) + "-" + strconv.Itoa(i-8)
+				}
+			} else {
+				start_row += 8
+			}
+		}
+	}
+	if output != "" {
+		return output
+	} else if valid_move != "" {
+		return valid_move
+	} else {
+		return "No found movements"
+	}
 }
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
@@ -178,6 +230,7 @@ func main() {
 	r := mux.NewRouter().StrictSlash(false)
 	r.HandleFunc("/", HomeHandler)
 	r.HandleFunc("/api/game", NextMoveHandler).Methods("POST")
+	r.HandleFunc("/api/board", CheckBoardHandler).Methods("POST")
 
 	server := &http.Server{
 		Addr: 		":8080",
